4abb40a7979cbbf6348e53f666fe1d06ccd60fe5
TeresaFlores <teresaflores1730@gmail.com>
2019-06-13 18:26:45 -0500

    Pase los test code y encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,31 @@
 window.cipher = {
-  // ... 
-};
+    encode:(num,tex)=>{
+      let code= '';
+      for (let i= 0; i < tex.length; i++){
+        let letter= tex.charCodeAt(i)
+    
+         const newASCII =(letter-65+num)%26+65;
+    
+         const newTex=(String.fromCharCode(newASCII))
+         code= code + newTex
+      } 
+        //  console.log(code)
+        return code;
+    },
+      // cepher(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toUpperCase());
+
+
+    decode: (numero,texto)=>{
+  let code1= '';
+  for (let i= 0; i < texto.length; i++){
+    let letter1= texto.charCodeAt(i)
+
+
+     const newASCII2 =(letter1+65-numero)%26+65;
+         
+     const newTexto=(String.fromCharCode(newASCII2));
+     code1= code1 + newTexto
+}
+return code1;
+    }
+  }
